c0a38b7b29dfb0c706241fbf6aa528f84f9a00cc
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-10-02 10:49:05 +0300

    .

--- src/main/java/com/tugalsan/lib/time/server/TS_LibTimeUtils.java
+++ src/main/java/com/tugalsan/lib/time/server/TS_LibTimeUtils.java
@@ -1,6 +1,7 @@
 package com.tugalsan.lib.time.server;
 
 import com.tugalsan.api.cast.client.TGS_CastUtils;
+import com.tugalsan.api.log.server.TS_Log;
 import com.tugalsan.api.optional.client.TGS_Optional;
 import com.tugalsan.api.string.client.TGS_StringUtils;
 import com.tugalsan.api.time.client.TGS_Time;
@@ -11,29 +12,51 @@ import java.util.StringTokenizer;
 
 public class TS_LibTimeUtils {
 
+    final private static TS_Log d = TS_Log.of(false, TS_LibTimeUtils.class);
+
     public static TGS_Optional<TGS_Time> fetch(CharSequence ip, int sslPort) {
         var url = TGS_LibTime.url(ip, sslPort);
         var urlData = TS_UrlDownloadUtils.toText(url, Duration.ofSeconds(10));
         if (TGS_StringUtils.isNullOrEmpty(urlData)) {
-            return TGS_Optional.ofEmpty("ERROR @ TS_LibTimeUtils.fetch", "REASON: TGS_StringUtils.isNullOrEmpty(urlData)");
+            return TGS_Optional.ofEmpty("ERROR@", d.className, "fetch",
+                    "REASON: TGS_StringUtils.isNullOrEmpty(urlData)"
+            );
         }
         var pattern = TGS_LibTime.PATTERN();
         if (pattern.length() != urlData.length()) {
-            return TGS_Optional.ofEmpty("ERROR @ TS_LibTimeUtils.fetch", "REASON: pattern.length() != urlData.length() where", "pattern", pattern);
+            return TGS_Optional.ofEmpty("ERROR@", d.className, "fetch",
+                    "REASON: pattern.length() != urlData.length() where",
+                    "pattern", pattern
+            );
         }
         var st = new StringTokenizer(" ");
         if (st.countTokens() != 2) {
-            return TGS_Optional.ofEmpty("ERROR @ TS_LibTimeUtils.fetch", "REASON: st.countTokens() != 2 where", "pattern", pattern, "urlData", urlData);
+            return TGS_Optional.ofEmpty("ERROR@", d.className, "fetch",
+                    "REASON: st.countTokens() != 2 where",
+                    "pattern", pattern,
+                    "urlData", urlData
+            );
         }
         var dateStr = st.nextToken();
         var dateLng = TGS_CastUtils.toLong(dateStr);
         if (dateLng == null) {
-            return TGS_Optional.ofEmpty("ERROR @ TS_LibTimeUtils.fetch", "REASON: dateLng == null where", "pattern", pattern, "urlData", urlData, "dateStr", dateStr);
+            return TGS_Optional.ofEmpty("ERROR@", d.className, "fetch",
+                    "REASON: dateLng == null where",
+                    "pattern", pattern,
+                    "urlData", urlData,
+                    "dateStr", dateStr
+            );
         }
         var timeStr = st.nextToken();
         var timeLng = TGS_CastUtils.toLong(timeStr);
         if (timeLng == null) {
-            return TGS_Optional.ofEmpty("ERROR @ TS_LibTimeUtils.fetch", "REASON: dateLng == null where", "pattern", pattern, "urlData", urlData, "dateStr", dateStr, "timeStr", timeStr);
+            return TGS_Optional.ofEmpty("ERROR@", d.className, "fetch",
+                    "REASON: dateLng == null where",
+                    "pattern", pattern,
+                    "urlData", urlData,
+                    "dateStr", dateStr,
+                    "timeStr", timeStr
+            );
         }
         return TGS_Optional.of(TGS_Time.ofDateAndTime(dateLng, timeLng));
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.tugalsan.lib.time {
     requires com.tugalsan.api.url;
     requires com.tugalsan.api.string;
     requires com.tugalsan.api.cast;
+    requires com.tugalsan.api.log;
     requires com.tugalsan.api.optional;
     exports com.tugalsan.lib.time.client;
     exports com.tugalsan.lib.time.server;
