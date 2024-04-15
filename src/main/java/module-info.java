2c30a258aea8afffc9dbc33eac22e18053e53f9d
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-15 14:25:46 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module com.tugalsan.lib.time {
     requires com.tugalsan.api.string;
     requires com.tugalsan.api.cast;
     requires com.tugalsan.api.log;
-    requires com.tugalsan.api.optional;
+    requires com.tugalsan.api.union;
     exports com.tugalsan.lib.time.client;
     exports com.tugalsan.lib.time.server;
 }
